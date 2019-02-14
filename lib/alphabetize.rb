c7fbbc998157f6ecc5d8e5fe524cf3e617c0db45
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-14 13:04:25 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,6 @@ def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |phrase| # sort the elements in arr by given condition
     phrase.split("").map do |word| 
-      return word
       esperanto.index(word)
     end
   end
